3ce23b947db3b5f2088b07a901a33b5af3e9bd9a
shalo1040 <kimd02@pfw.edu>
2020-07-17 08:37:37 +0900

    [Daeun] 최근 본 책방 통신 연결 중

--- app/src/main/java/com/example/cozy/network/responseData/ResponseRecent.kt
+++ app/src/main/java/com/example/cozy/network/responseData/ResponseRecent.kt
@@ -1,2 +1,15 @@
 package com.example.cozy.network.responseData
 
+data class ResponseRecent(
+    val status : Int,
+    val success : Boolean,
+    val message : String,
+    val data : List<RecentlySeenData>?
+)
+
+data class RecentlySeenData(
+    val bookstoreIdx : Int,
+    val bookstoreName : String,
+    val profile : String,
+    val image1 : String
+)
