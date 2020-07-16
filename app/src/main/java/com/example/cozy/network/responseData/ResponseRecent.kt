a275f1de46e0de843cc2835230f7427a0c846a3b
shalo1040 <kimd02@pfw.edu>
2020-07-17 08:27:16 +0900

    [Daeun] mapView에서 검색 페이지로 전환

--- app/src/main/java/com/example/cozy/network/responseData/ResponseRecent.kt
+++ app/src/main/java/com/example/cozy/network/responseData/ResponseRecent.kt
@@ -1,10 +1,2 @@
 package com.example.cozy.network.responseData
 
-import com.example.cozy.views.mypage.RecentlySeenData
-
-data class ResponseRecent (
-    val status : Int,
-    val success : Boolean,
-    val message : String,
-    val data : List<RecentlySeenData>
-)
--- app/src/main/java/com/example/cozy/views/mypage/RecentlySeenData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cozy.views.mypage
-
-data class RecentlySeenData(
-    val bookstoreIdx : Int,
-    val bookstoreName: String,
-    val profile : String,
-    val image1 : String
-)
